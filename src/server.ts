6dc96c7e2eadb028da1d8f030cedabf6ded2c663
Beth <bethan.hp@gmail.com>
2023-08-15 10:19:04 +0100

    server set up

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import dotenv from "dotenv";
 import express from "express";
 import { Client } from "pg";
 import { getEnvVarOrFail } from "./support/envVarUtils";
-import { setupDBClientConfig } from "./support/setupDBClientConfig";
 
 dotenv.config(); //Read .env file lines as though they were env vars.
 
-const dbClientConfig = setupDBClientConfig();
-const client = new Client(dbClientConfig);
+const client = new Client({ connectionString: process.env.DATABASE_URL });
+if (process.env.DATABASE_URL === undefined) {
+    throw new Error("DATABASE_URL is undefined!");
+}
 
 //Configure express routes
 const app = express();
@@ -16,9 +17,113 @@ const app = express();
 app.use(express.json()); //add JSON body parser to each following route handler
 app.use(cors()); //add CORS support to each following route handler
 
-app.get("/", async (req, res) => {
-    res.json({ msg: "Hello! There's nothing interesting for GET /" });
+// get a random quote
+app.get("/quotes/1", async (req, res) => {
+    try {
+        const CountAll = "SELECT COUNT(*) from mot_quotes";
+        const total = await client.query(CountAll);
+        const random_id = Math.floor(
+            Math.random() * parseInt(total.rows[0].count)
+        );
+        const queryForRandom = "Select * from mot_quotes where id = $1";
+        const random = await client.query(queryForRandom, [random_id]);
+        res.status(200).json(random.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Something went wrong");
+    }
 });
+
+// get random 5 quotes
+app.get("/quotes/5", async (req, res) => {
+    try {
+        const CountAll = "SELECT COUNT(*) from mot_quotes";
+        const total = await client.query(CountAll);
+        const random_idList = () => {
+            const arrOfIds = [];
+            for (let i = 0; i < 5; i++) {
+                arrOfIds.push(
+                    Math.floor(Math.random() * parseInt(total.rows[0].count))
+                );
+            }
+            return arrOfIds;
+        };
+        const queryForRandom =
+            "Select * from mot_quotes where id in ($1, $2, $3, $4, $5)";
+        const random = await client.query(queryForRandom, random_idList());
+        res.status(200).json(random.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Something went wrong");
+    }
+});
+
+// get all favourite quotes
+app.get("/quotes/favourites", async (req, res) => {
+    try {
+        const all = "SELECT * from favourite_quotes";
+        const allItems = await client.query(all);
+        res.status(200).json(allItems.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Something went wrong");
+    }
+});
+
+// add quote to favourites
+app.post<{}, {}, { id: string }>("/quotes/favourites", async (req, res) => {
+    try {
+        const find = await client.query(
+            "select * from mot_quotes where id = $1",
+            [parseInt(req.body.id)]
+        );
+        const insert =
+            "insert into favourite_quotes (quote_id, quote, author) values ($1, $2, $3)";
+        const insertValues = [
+            find.rows[0].id,
+            find.rows[0].quote,
+            find.rows[0].author,
+        ];
+        const add = await client.query(insert, insertValues);
+
+        res.json("Quote favourited");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(
+            "Something went wrong or quote has already been favourited!"
+        );
+    }
+});
+
+// remove quote from favourites
+app.delete("/quotes/favourites/:id", async (req, res) => {
+    try {
+        const del = "delete from favourite_quotes where quote_id = $1";
+        const delValue = [parseInt(req.body.id)];
+        const unFav = await client.query(del, delValue);
+        res.json("Quote unfavourited");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Something went wrong");
+    }
+});
+
+// add quote to db - can add duplicates
+app.post<{}, {}, { quote: string; author: string }>(
+    "/quotes",
+    async (req, res) => {
+        try {
+            const insert =
+                "insert into mot_quotes (quote, author) values ($1, $2)";
+            const insertValues = [req.body.quote, req.body.author];
+            const add = await client.query(insert, insertValues);
+            res.json("Quote added");
+        } catch (err) {
+            console.error(err);
+            res.status(500).send("Something went wrong");
+        }
+    }
+);
 
 app.get("/health-check", async (req, res) => {
     try {
