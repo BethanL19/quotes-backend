4e8c4f20eab18506840d16b854e581f058711809
Beth <bethan.hp@gmail.com>
2023-08-15 11:00:10 +0100

    files removed

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import dotenv from "dotenv";
 import express from "express";
 import { Client } from "pg";
 import { getEnvVarOrFail } from "./support/envVarUtils";
-import { setupDBClientConfig } from "./support/setupDBClientConfig";
 
 dotenv.config(); //Read .env file lines as though they were env vars.
 
-const dbClientConfig = setupDBClientConfig();
-const client = new Client(dbClientConfig);
+const client = new Client({ connectionString: process.env.DATABASE_URL });
+if (process.env.DATABASE_URL === undefined) {
+    throw new Error("DATABASE_URL is undefined!");
+}
 
 //Configure express routes
 const app = express();
